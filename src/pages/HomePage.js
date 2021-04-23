d027357bc50638691f7049afc170b2bf1936e22d
james <engrjayt200@gmail.com>
2021-04-23 14:12:34 +0100

    Update Navbar component

---
+++ src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <main>
+       <h2>I am coding fine</h2>
+            
+        </main>
+    );
+}
+
+export default HomePage;
