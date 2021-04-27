c294131c84ee0db8b323dc62d3353a097498b33b
james <engrjayt200@gmail.com>
2021-04-27 01:17:45 +0100

    Added CartContext

---
+++ src/pages/CheckoutPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import styled from "styled-components";
+import { PageHero, StripeCheckout } from "../components";
+
+import { useCartContext } from "../context/"
+
+const CheckoutPage = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default CheckoutPage;
